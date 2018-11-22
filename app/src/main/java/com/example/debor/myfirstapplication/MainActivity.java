03b4a29d6dd4c36a58cc3d0bd84b98cf360da20f
Natasha S Love <debora@inbox.ru>
2018-11-22 01:16:48 -0600

    fixed error (rename)

--- app/src/main/java/com/example/debor/myfirstapplication/MainActivity.java
+++ app/src/main/java/com/example/debor/myfirstapplication/MainActivity.java
@@ -20,7 +20,7 @@ public class MainActivity extends AppCompatActivity {
     public void sendMessage(View view) {
         // Do something in response to button
         Intent intent = new Intent(this, DisplayMessageActivity.class);
-        EditText editText = (EditText) findViewById(R.id.editText);
+        EditText editText = (EditText) findViewById(R.id.editText2);
         String message = editText.getText().toString();
         intent.putExtra(EXTRA_MESSAGE, message);
         startActivity(intent);
